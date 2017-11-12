14064234e1d4ce83d231c630a387035ea798e162
Sufien Tout <sufientout@gmail.com>
2017-11-12 13:19:21 -0500

    Added JSON Parsing support

--- src/graphics/Codex.hpp
+++ src/graphics/Codex.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <string>
+#include <fstream>
 #include <unordered_map>
 #include <memory>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <json.hpp>
 
 class Codex {
 private:
@@ -12,16 +14,19 @@ private:
     static std::unordered_map<std::string, std::shared_ptr<sf::Image>> images;
     static std::unordered_map<std::string, std::shared_ptr<sf::Font>> fonts;
     static std::unordered_map<std::string, std::shared_ptr<sf::SoundBuffer>> sounds;
+    static std::unordered_map<std::string, std::shared_ptr<nlohmann::json>> jsons;
 
 public:
     static std::shared_ptr<sf::Texture> AcquireTexture(const std::string& name);
     static std::shared_ptr<sf::Image> AcquireImage(const std::string& name);
     static std::shared_ptr<sf::Font> AcquireFont(const std::string& name);
     static std::shared_ptr<sf::SoundBuffer> AcquireSound(const std::string& name);
+    static std::shared_ptr<nlohmann::json> AcquireJson(const std::string& name);
     static void Clean();
 private:
     static void CleanTextures();
     static void CleanImages();
     static void CleanFonts();
     static void CleanSounds();
+    static void CleanJsons();
 };
